193edeb4a05b356768ebad415e2e4c56d4a822ef
gistrec <gistrec@mail.ru>
2021-05-03 21:17:08 +0700

    Added verbose option and improved output

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const {Builder, By, Key} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const proxy = require('selenium-webdriver/proxy');
+const yargs = require('yargs');
 
 const { getExtension, getAddHeaderUrl } = require('firefox-modheader');
 
-
-const yargs = require('yargs');
 
 const argv = yargs
     .demandOption(['proxy-address', 'proxy-auth', 'vk-auth'])
     .option('proxy-address', { description: 'Proxy data in format ip:port' })
     .option('proxy-auth',    { description: 'Authentication data in format login:password' })
     .option('vk-auth',       { description: 'Authentication data in format login:password' })
+    .option('verbose',       { description: 'Show verbose output'})
+    .default('verbose', false)
     .help(false)
     .version(false)
     .argv;
+
+const verbose = argv['verbose'];
 
 (async function run() {
     const options = new firefox.Options();
     options.addExtensions(getExtension());
     options.headless();
 
+    if (verbose) console.log("Initial driver.");
     const driver = await new Builder()
         .forBrowser('firefox')
         .setFirefoxOptions(options)
@@ -29,36 +33,47 @@ const argv = yargs
             https: argv['proxy-address']
         }))
         .build();
+    if (verbose) console.log("Driver has been initialized.");
 
     try {
         await driver.get(getAddHeaderUrl('Proxy-Authorization', 'Basic ' + Buffer.from(argv['proxy-auth']).toString('base64')));
-        await driver.sleep(1000);
-        console.debug('Set Proxy-Authorization header')
+        if (verbose) console.log('Proxy-Authorization header has been set.');
 
+        await driver.sleep(1000);
         await driver.get('https://vk.com');
-        console.debug('Page vk.com loaded')
+        if (verbose) console.log('Page vk.com has been loaded.');
 
+        await driver.sleep(1000);
         await driver.findElement(By.id('index_email')).sendKeys(argv['vk-auth'].split(':')[0]);
-        await driver.sleep(1000);
-
         await driver.findElement(By.id('index_pass')).sendKeys(argv['vk-auth'].split(':')[1], Key.ENTER);
-        await driver.sleep(1000);
-        console.debug('Login/Password wrote.')
+        if (verbose) console.log('Login and password have been written.')
 
+        await driver.sleep(1000);
         const currentUrl = await driver.getCurrentUrl();
         if (currentUrl.indexOf('vk.com/login') != -1) {
-            console.warn('Login/Password are incorrect.');
+            console.warn('Password incorrect.');
             process.exit(1);
         }
 
-        const cookies = await driver.manage().getCookies();
-        const string = cookies.map(cookie => {
-            return `${cookie.name}=${cookie.value}`;
-        }).join('; ');
+        await driver.sleep(1000);
+        await driver.get('https://vk.com/id0');
 
-        console.log(string);
+        const cookies = await driver.manage().getCookies();
+        const name    = await driver.findElement(By.className('page_name')).getText();
+        const url     = await driver.getCurrentUrl();
+
+        console.log(JSON.stringify({
+            status: 'success',
+            cookies: cookies.map(cookie => cookie.name + '=' + cookie.value).join('; '),
+            name,
+            url
+        }));
+
     } catch (err) {
-        console.error(err);
+        console.log(JSON.stringify({
+            status: 'failed',
+            error: err
+        }));
         process.exit(2);
     }
 })();
